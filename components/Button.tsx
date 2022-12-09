c7f27dfc57d98482479f41bc413f22ee974a3573
Alef Barbeli <alef.barbeli@digitalboutique.co.uk>
2022-12-09 19:22:47 -0300

    added tailwind components

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border(gray-100 2) hover:bg-gray-200"
+      class="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
     />
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,45 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { ProductCard } from "../components/ProductCard.tsx";
 
-export default function Home() {
+export default function Index() {
   return (
     <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-12 mx-auto">
+          <h1 className="text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white">Portfolio</h1>
+
+          <div className="mt-8 xl:mt-16 lg:flex lg:-mx-12">
+            <div className="lg:mx-12">
+              <h1 className="text-xl font-semibold text-gray-800 dark:text-white">Table of Content</h1>
+
+              <div className="mt-4 space-y-4 lg:mt-8">
+                <a href="#" className="block text-blue-500 dark:text-blue-400 hover:underline">Web design</a>
+                <a href="#" className="block text-gray-500 dark:text-gray-300 hover:underline">App design</a>
+                <a href="#" className="block text-gray-500 dark:text-gray-300 hover:underline">Branding</a>
+                <a href="#" className="block text-gray-500 dark:text-gray-300 hover:underline">Animation</a>
+              </div>
+            </div>
+
+            <div className="flex-1 mt-8 lg:mx-12 lg:mt-0">
+              <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 ">
+                <ProductCard
+                  imgURL="https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"
+                  name="Best website collections"
+                />
+
+                <ProductCard
+                  imgURL="https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                  name="Block of Ui kit collections"
+                />
+
+                <ProductCard
+                  imgURL="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  name="Ton’s of mobile"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
